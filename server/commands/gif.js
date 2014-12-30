8516672b7c480132639bd288a34eda4b371d6b89
Jared Schmidt <jschmidt@problemsolutions.net>
2014-12-30 09:48:52 -0500

    gif keyword search

--- server/commands/gif.js
+++ server/commands/gif.js
@@ -2,14 +2,15 @@ gif = {
     start:function(slack){
         var offset = Math.floor((Math.random() * 10) + 1); //random number between 1 and 10
         var giphy_token = Meteor.settings['giphy_public_token'];
-        var url = 'http://api.giphy.com/v1/gifs/translate?'+q_string({'s':slack['text']})+'&api_key='+giphy_token+'&limit=1&offset='+offset
+        var url = 'http://api.giphy.com/v1/gifs/search?'+q_string({'q':slack['text']})+'&api_key='+giphy_token+'&limit=1&offset='+offset
+        console.log(url);
         var j_data = get_call(url);
         var limit = 10;
         try{
             People.update({'id': slack['slack_id']}, {$inc:{"gifs": 1}});
             var person = People.findOne({'id':slack['slack_id']});
             if (person.gifs <= limit){
-                var url = j_data['data']['images']['original']['url'];
+                var url = j_data['data'][0]['images']['original']['url'];
                 outgoing_bot('called by: '+ slack['slack_name'] + " Searched: " + slack['text'] +" " + url, slack['channel_id']);
                 message = "Sent. " + (limit - person.gifs).toString() + " Gifs left.";
             }else{
