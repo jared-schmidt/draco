1a0ebbb1a2a9daf0878865f1f1dffe3270ff3548
Jared Schmidt <jschmidt@problemsolutions.net>
2015-01-07 11:54:06 -0500

    help function

--- server/commands/help.js
+++ server/commands/help.js
@@ -1,20 +1,26 @@
 help = {
     start:function(slack){
         message = '\n';
-        message += 'gif {text} - Returns gif based on text. \n';
-        message += 'sticker {text} - Returns sticker based on text. \n';
-        message += 'star {username} - Gives a goldstar to a person. \n';
-        message += 'mystars - Gives you number of stars you have. \n';
-        message += 'table - Gif of flipping a table. \n';
-        message += 'meme {toptext|bottomText} - Meme with text. \n';
-        message += 'face {text} - Returns a face? \n';
-        message += 'robot {text} - Returns a robot generatered from your text. \n';
-        message += 'cookie - Returns a fortune cookie message. \n';
-        message += 'game question - Returns a question for anyone to answer. \n';
-        message += 'game answer {text} - Submits an answer for the question. \n';
-        message += 'steve {text*} - Returns a random meme picture of Steve. \n';
-        message += 'yoda {text} - Returns talk like yoda. \n';
-        message += 'about - about. \n';
+
+        // message += 'about - about. \n';
+
+        var mycommands = JSON.parse(Assets.getText("commands.json"));
+        keys = Object.keys(mycommands['commands']);
+        keys.sort();
+        var len = keys.length;
+
+        for(var i=0; i<len; i++){
+            k = keys[i];
+            var obj = mycommands['commands'][k];
+            if (obj['active'] && !obj['hidden']){
+                var extra = '';
+                if(obj.hasOwnProperty('extra')){
+                    extra = '{'+ obj['extra'] + '}';
+                }
+                message += key + ' ' + extra +' - ' + obj['what'] + '\n';
+            }
+        }
+
         return message;
     }
 }
