e6304c768da65b95f992b020a1e4264496515e6f
Jared Schmidt <jschmidt@problemsolutions.net>
2015-01-13 14:03:59 -0500

    fixed name

--- server/commands/chuck.js
+++ server/commands/chuck.js
@@ -3,7 +3,7 @@ chuck = {
     var url = 'http://api.icndb.com/jokes/random';
     var j_data = get_call(url);
     try{
-      other_bot(j_data['value']['joke'], slack['channel_id'], "Chuck", ":muscel:")
+      other_bot(j_data['value']['joke'], slack['channel_id'], "Chuck", ":muscle:")
       message = 'Sent';
     }catch(err){
       message = 'failed to find chuck.';
